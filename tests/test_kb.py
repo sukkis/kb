7a18f81085574004012e91fcf550ae68384dd295
Petteri Suckman <petteri.suckman@gmail.com>
2021-09-01 18:25:22 +0300

    Fixed broken help text assertion.

--- tests/test_kb.py
+++ tests/test_kb.py
@@ -9,8 +9,16 @@ import re
 import kb.kb as kb
 
 
-def test_display_content():
+def test_display_content(capfd):
     """Test if help text is returned."""
-    help_contents = str(kb.display_content("--help", "/home/$USER/kb/"))
+    kb.display_content("--help", "/home/$USER/kb/")
+    captured = capfd.readouterr()
     help_regex = re.compile(r'Find snippets')
-    assert help_regex.match(help_contents)
+    assert help_regex.match(captured.out)
+
+def setup_function(function):
+    print("setting up", function)
+
+def test_func1():
+    """Always pass"""
+    assert True
