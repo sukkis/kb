5872ebe3142f6b003419330d58db26ee67eb49fa
Petteri Suckman <petteri.suckman@gmail.com>
2021-09-04 11:14:43 +0300

    Added system tests.

--- tests/test_kb.py
+++ tests/test_kb.py
@@ -1,17 +1,24 @@
 """Unit test module kb.
 
     Functions:
-    test_display_contents()
+    test_display_content()
     test_add_kb_entry()
 """
 
 import re
+import os
 from kb import kb
+
+
+# Constants
+DIR = os.path.expandvars("/home/$USER/kb/")
+HOME = os.path.expandvars("/home/$USER")
+TEMP_FILE = HOME + "/temp/search"
 
 
 def test_display_content(capfd):
     """Test if help text is returned."""
-    kb.display_content("--help", "/home/$USER/kb/")
+    kb.display_content("--help", DIR)
     captured = capfd.readouterr()
     help_regex = re.compile(r"Find snippets")
     assert help_regex.match(captured.out)
